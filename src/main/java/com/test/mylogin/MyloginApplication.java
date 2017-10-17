215c649832f6ed5e7f2f9bb2a36e0e1152fb87fd
git-sir <369634457@qq.com>
2017-10-17 21:25:43 +0800

    pom文件内容为war包配置。学习文档章节"部署-运行war包"增加SpringBootServletInitializer说明。

--- src/main/java/com/test/mylogin/MyloginApplication.java
+++ src/main/java/com/test/mylogin/MyloginApplication.java
@@ -2,12 +2,19 @@ package com.test.mylogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyloginApplication {
+public class MyloginApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyloginApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
